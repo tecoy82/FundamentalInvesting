433fc3897e925e383bba44ba87f20b0b0e474b46
timcoyle <timcoyle.purchases@gmail.com>
2018-03-15 21:21:16 -0400

    Skip if no price book ratio in Yahoo

--- FundamentalInvesting.py
+++ FundamentalInvesting.py
@@ -9,13 +9,13 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def yahoo_key_stats(stock):
     try:
         source_code = urlopen('http://finance.yahoo.com/q/ks?s='+stock).read().decode("utf-8")
-        left_pbr = '<span data-reactid="57">Price/Book</span><!-- react-text: 58 --> <!-- /react-text --><!-- react-text: 59 -->(mrq)<!-- /react-text --><sup aria-label="KS_HELP_SUP_undefined" data-reactid="60"></sup></td><td class="Fz(s) Fw(500) Ta(end)" data-reactid="61">'
-        right_pbr = '</td></tr><tr data-reactid="62"><td data-reactid="63">'
-        price_book_ratio = source_code.split(left_pbr)[1].split(right_pbr)[0]
+        price_book_ratio = source_code.split('Price/Book')[1].split('Fz(s) Fw(500) Ta(end)')[1].split('>')[1].split('<')[0]
+        price_earnings_to_5yr_growth = source_code.split('PEG Ratio (5 yr expected)')[1].split('Fz(s) Fw(500) Ta(end)')[1].split('>')[1].split('<')[0]
 
-        # print("{} has a Price Book Ratio of {}".format(stock, price_book_ratio))
-        if float(price_book_ratio) < 5:
-            print("{} has a Price Book Ratio of {}".format(stock, price_book_ratio))
+        # print(type(price_book_ratio), price_book_ratio)
+        if price_book_ratio != 'N/A' and price_book_ratio != '':
+            if float(price_book_ratio) < 5:
+                print("{} has a Price Book Ratio of {} and a Price/Earnings to 5yr Growth of {}".format(stock, price_book_ratio, price_earnings_to_5yr_growth))
 
     except Exception as e:
         print("Failed in the main loop", str(e))
@@ -27,9 +27,10 @@ def main():
     daily_sp500 = util.get_snp500()
     for sector, ticker in daily_sp500.items():
         for symbol in daily_sp500[sector]:
-            if count > 12: break
+            if count > 15: break
+            print("{}:{}".format(count,symbol))
             yahoo_key_stats(symbol)
-            time.sleep(0.5)
+            time.sleep(0.25)
             count += 1
 
 
